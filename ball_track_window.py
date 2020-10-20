b75a4f5974eed18fa9bc2647a29c03278e921304
Fatma Nur Arabacı <32905215+fatooo@users.noreply.github.com>
2020-10-20 08:37:19 +0300

    30 fps array

--- ball_track_window.py
+++ ball_track_window.py
@@ -8,23 +8,27 @@ import matplotlib.pyplot as plt
 
 start_time = time.time()
 
-img_dir = "C:\\Users\\fatma\\Desktop\\Bilardo\\Pool_Table_Docs\\20fps"  # Enter Directory of all images
+img_dir = "C:\\Users\\fatma\\Desktop\\Bilardo\\Pool_Table_Docs\\30fps"  # Enter Directory of all images
 data_path = os.path.join(img_dir, '*g')
 files = glob.glob(data_path)
 data = []
 initial_window = 300
-fps = 20 #frames per second
+fps = 30 #frames per second
 ppm = 370 #pixel per meter
 w = initial_window
-template = cv2.imread('white_ball.png')
+template = cv2.imread('white_big.png')
 template = np.float32(template)
-frame_amount = 152  #152 for 20fps & 39 for 5 fps & 43 for 5fps new
+template_x = template.shape[0]
+template_y = template.shape[1]
+#print(template_x,template_y)
+frame_amount = 225  #152 for 20fps & 39 for 5 fps & 43 for 5fps new & 225 for 30fps
 
 method = cv2.TM_SQDIFF  #method used to check the correlation between the template and the frame
 
 for f1 in files:
     img = cv2.imread(f1)
     data.append(img)
+    #cv2.imshow('deneme', data[i]), cv2.waitKey(0), cv2.destroyAllWindows()
 
 min_val = [0] * frame_amount
 max_val = [0] * frame_amount
@@ -59,7 +63,6 @@ for i in range(frame_amount):
             x_stop = x_start + w
         # print(x_start,x_stop,y_start,y_stop)
         img_crop = data[i][int(x_start):int(x_stop), int(y_start):int(y_stop)]
-    #cv2.imshow('final', img_crop),cv2.waitKey(0),cv2.destroyAllWindows()
     img_crop = np.float32(img_crop)
     res = cv2.matchTemplate(img_crop, template, method)
     min_val[i], max_val[i], min_loc[i], max_loc[i] = cv2.minMaxLoc(res)
@@ -78,7 +81,7 @@ for i in range(frame_amount):
             y_prime[i] = y_prime[i - 1] - half_window + min_loc[i][0]
 
         #print(y_prime[i], x_prime[i])
-        template = data[i][int(x_prime[i]):int(x_prime[i])+30, int(y_prime[i]):int(y_prime[i])+30] #update the template with the ball found in the current frame
+        template = data[i][int(x_prime[i]):int(x_prime[i])+template_x, int(y_prime[i]):int(y_prime[i])+template_y] #update the template with the ball found in the current frame
         #cv2.imshow('final', template), cv2.waitKey(0), cv2.destroyAllWindows() #run to see the template used for the search in the next frame
         template = np.float32(template)
 
@@ -99,8 +102,11 @@ for i in range(frame_amount):
 for i in range(frame_amount):
     if i - 1 == -1:
         i = i + 1
-    cv2.line(data[frame_amount-1], (int(y_prime[i-1]+15), int(x_prime[i-1]+15)), (int(y_prime[i]+15), int(x_prime[i]+15)), (255, 255, 255), 1)
-    cv2.circle(data[frame_amount-1], (int(y_prime[i-1])+15, int(x_prime[i-1])+15), 2, (0, 0, 255), -1)
+    cv2.line(data[frame_amount-1], (int(y_prime[i-1]+int(template_y/2)+1), int(x_prime[i-1]+int(template_x/2)+1)), (int(y_prime[i]+int(template_y/2)+1), int(x_prime[i]+int(template_x/2)+1)), (255, 255, 255), 1)
+    cv2.circle(data[frame_amount-1], (int(y_prime[i])+int(template_y/2)+1, int(x_prime[i])+int(template_x/2)+1), 2, (0, 0, 255), -1)
+    rect = cv2.rectangle(data[i], (int(y_prime[i]), int(x_prime[i])), (int(y_prime[i]+template_y), int(x_prime[i]+template_x)), (0, 255, 0),1)
+    #cv2.imshow('final', data[i]), cv2.waitKey(0), cv2.destroyAllWindows()
 
 print("--- %s seconds ---" % (time.time() - start_time))
-cv2.imshow('final', data[frame_amount-1]),cv2.waitKey(0),cv2.destroyAllWindows()
+
+cv2.imshow('final', data[frame_amount-1]), cv2.waitKey(0),cv2.destroyAllWindows()
