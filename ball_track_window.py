525c9e51406cb1ec3ffe2a727cb2fca9e7f01fae
Fatma Nur Arabacı <32905215+fatooo@users.noreply.github.com>
2020-10-26 09:30:11 +0300

    Last commit before hough trials

--- ball_track_window.py
+++ ball_track_window.py
@@ -8,16 +8,17 @@ import matplotlib.pyplot as plt
 
 start_time = time.time()
 
-img_dir = "C:\\Users\\fatma\\Desktop\\Bilardo\\Pool_Table_Docs\\20fps"  # Enter Directory of all images
+img_dir = "C:\\Users\\fatma\\Desktop\\Bilardo\\Pool_Table_Docs\\video_data2"  # Enter Directory of all images
 data_path = os.path.join(img_dir, '*g')
 files = glob.glob(data_path)
 data = []
+big_templates = []
 initial_window = 200
-fps = 20 #frames per second
+fps = 30 #frames per second
 ppm = 370 #pixel per meter
 w = initial_window
-template_o = cv2.imread('white_big.png')
-template_2_o = cv2.imread('white_ball_sym.png')
+template_o = cv2.imread('salon_white_big.png')
+template_2_o = cv2.imread('white_salon.png')
 #template = cv2.GaussianBlur(template,(9,9),-1)
 template = np.float32(template_o)
 template_2= np.float32(template_2_o)
@@ -26,11 +27,13 @@ template_y = template.shape[1]
 #print(template_x,template_y)
 template_x_2 = template_2.shape[0]
 template_y_2 = template_2.shape[1]
+#template_x_2 = 0
+#template_y_2 = 0
 #print(template_x_2,template_y_2)
 
-frame_amount = 152  #152 for 20fps & 39 for 5 fps & 43 for 5fps new & 225 for 30fps
+frame_amount = 422  #152 for 20fps & 39 for 5 fps & 43 for 5fps new & 225 for 30fps & 422 for salon data
 
-method = cv2.TM_SQDIFF  #method used to check the correlation between the template and the frame
+method = cv2.TM_SQDIFF_NORMED  #method used to check the correlation between the template and the frame
 
 for f1 in files:
     img = cv2.imread(f1)
@@ -86,7 +89,7 @@ for i in range(frame_amount):
     est_x = int(min_loc[i][1] + velocity_x[i-1] * (1 / fps))
     est_y = int(min_loc[i][0] + velocity_y[i-1] * (1 / fps))
 
-    # print("found ball",min_loc[i])
+    #print("Estimated position: ",est_x,est_y)
     if i == 0:
         x_prime[i] = est_x + 11 #why 11 & 17?
 
@@ -102,21 +105,31 @@ for i in range(frame_amount):
             y_prime[i] = y_prime[i - 1] - half_window + est_y
 
         #print("1",y_prime[i], x_prime[i])
+
         template_o = data[i][int(x_prime[i]):int(x_prime[i])+template_x, int(y_prime[i]):int(y_prime[i])+template_y] #update the template with the ball found in the current frame
-        template_o = cv2.GaussianBlur(template_o, (5, 5), -1)
+        #template_o = cv2.GaussianBlur(template_o, (5, 5), -1)
+
         #print(template_x, template_y)
+        big_templates.append(template_o)
+
         #cv2.imshow('final', template_o), cv2.waitKey(0), cv2.destroyAllWindows() #run to see the template used for the search in the next frame
+
         template = np.float32(template_o)
         res_2 = cv2.matchTemplate(template, template_2, method)
+
         min_val_2[i], max_val_2[i], min_loc_2[i], max_loc_2[i] = cv2.minMaxLoc(res_2)
         #print(min_loc_2[i][1],min_loc_2[i][0])
-        cv2.circle(template_o, (int(min_loc_2[i][0]+1) + int(template_y_2 / 2), int(min_loc_2[i][1]) + int(template_x_2 / 2)), 2,
-                   (0, 0, 255), -1)
-        rect = cv2.rectangle(template_o, (int(min_loc_2[i][0]+2), int(min_loc_2[i][1])),
-                             (int(min_loc_2[i][0] + template_y_2), int(min_loc_2[i][1] + template_x_2)), (0, 255, 0), 1)
+
+        #cv2.circle(template_o, (int(min_loc_2[i][0]+1) + int(template_y_2 / 2), int(min_loc_2[i][1]) + int(template_x_2 / 2)), 2,
+                   #(0, 0, 255), -1)
+        #rect = cv2.rectangle(template_o, (int(min_loc_2[i][0]+2), int(min_loc_2[i][1])),
+                             #(int(min_loc_2[i][0] + template_y_2), int(min_loc_2[i][1] + template_x_2)), (0, 255, 0), 1)
         #cv2.imshow('final', template_o), cv2.waitKey(0), cv2.destroyAllWindows()
-        x_prime[i] = x_prime[i] + template_x / 2 - template_x_2 / 2 + min_loc_2[i][1]
+
+
+        x_prime[i] = x_prime[i] + template_x / 2 - template_x_2 / 2 + min_loc_2[i][1] #used for double template matching
         y_prime[i] = y_prime[i] + template_y / 2 - template_y_2 / 2 + min_loc_2[i][0]
+
         #print("2",y_prime[i], x_prime[i])
         #print("--------------")
 
@@ -124,7 +137,7 @@ for i in range(frame_amount):
     velocity_x[i] = abs(x_prime[i] - x_prime[i - 1])/fps
     velocity_y[i] = abs(y_prime[i] - y_prime[i - 1])/fps
 
-    #print(velocity_x[i],velocity_x[i])
+    #print(velocity_x[i],velocity_y[i])
     #print(w)
 #velocity[0] = 0
 #plt.plot(velocity),plt.ylabel('Velocity (m/s)'),plt.xlabel('Frame'),plt.show()
@@ -133,11 +146,27 @@ for i in range(frame_amount):
     if i - 1 == -1:
         i = i + 1
     #print(y_prime[i-1],x_prime[i-1])
-    cv2.line(data[frame_amount-1], (int(y_prime[i-1]-(int(template_y/2)-int(template_y_2/2))+int(template_y_2/2)), int(x_prime[i-1]-(int(template_x/2)-int(template_x_2/2))+template_x_2/2)), (int(y_prime[i]-(int(template_y/2)-int(template_y_2/2))+int(template_y_2/2)), int(x_prime[i]-(int(template_x/2)-int(template_x_2/2))+template_x_2/2)), (255, 255, 255), 1)
-    cv2.circle(data[frame_amount-1],  (int(y_prime[i]-(int(template_y/2)-int(template_y_2/2))+int(template_y_2/2)),int(x_prime[i]-(int(template_x/2)-int(template_x_2/2))+template_x_2/2)), 2, (0, 0, 255), -1)
+    center_prev = (int(y_prime[i-1]-(int(template_y/2)-int(template_y_2/2))+int(template_y_2/2)), int(x_prime[i-1]-(int(template_x/2)-int(template_x_2/2))+template_x_2/2))
+    center = (int(y_prime[i]-(int(template_y/2)-int(template_y_2/2))+int(template_y_2/2)), int(x_prime[i]-(int(template_x/2)-int(template_x_2/2))+template_x_2/2))
+    #center_prev = (int(y_prime[i-1]+(int(template_y/2))), int(x_prime[i-1]+(int(template_x/2))))
+    #center = (int(y_prime[i]+(int(template_y/2))), int(x_prime[i]+(int(template_x/2))))
+    cv2.line(data[i], center_prev , center , (255, 255, 255), 1)
+    cv2.circle(data[i],  (center[0],center[1]), 2, (0, 0, 255), -1)
+    #print("Actual position: ",center[0], center[1])
     #rect = cv2.rectangle(data[i], (int(y_prime[i]-(int(template_y/2)-int(template_y_2/2))),int(x_prime[i]-(int(template_x/2)-int(template_x_2/2)))), (int(y_prime[i]-(int(template_y/2)-int(template_y_2/2))+template_y_2),int(x_prime[i]-(int(template_x/2)-int(template_x_2/2))+template_x_2)), (0, 255, 0),1)
-    #cv2.imshow('final', data[i]), cv2.waitKey(0), cv2.destroyAllWindows()
+    cv2.rectangle(data[i],((center[0]-int(template_y/2)),center[1]-int(template_x/2)),((center[0]+int(template_y/2)),center[1]+int(template_x/2)), (0, 255, 0),2)
+    cv2.rectangle(data[i], ((center[0] - int(template_y_2 / 2)), center[1] - int(template_x_2 / 2)),
+                         ((center[0] + int(template_y_2 / 2)), center[1] + int(template_x_2 / 2)), (0, 255, 0), 2)
+    #path = 'C:\\Users\\fatma\\Desktop\\Bilardo\\Pool_Table_Docs\\processed_data'
+    #cv2.imwrite(os.path.join(path, 'img_%d.jpg' %i), imS)
+    data[i][(center[1]-int(template_x_2/2)-1):(center[1]+int(template_x_2/2)),(center[0]+int(template_y_2/2)):(center[0]+int(3*template_y_2/2)),:] = template_2_o
+    cv2.rectangle(data[i],(center[0]+int(template_y_2/2),center[1]-int(template_x_2/2)),((center[0]+int(3*template_y_2/2)),(center[1]+int(template_x_2/2))), (0, 255, 0),2)
+
+    #data[i][950:950+template_x_2, 1800:1800+template_y_2, :] = template_2_o
+    data[i][950:950+template_x, 1700:1700+template_y, :] = big_templates[i]
+    imS = cv2.resize(data[i], (1200, 706))
+    cv2.imshow('final', imS), cv2.waitKey(0), cv2.destroyAllWindows()
 
 print("--- %s seconds ---" % (time.time() - start_time))
-
-cv2.imshow('final', data[frame_amount-1]), cv2.waitKey(0),cv2.destroyAllWindows()
+imS = cv2.resize(data[frame_amount-1], (960, 540))
+cv2.imshow('final', imS), cv2.waitKey(0),cv2.destroyAllWindows()
